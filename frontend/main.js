08a069b4106b4db7735952f88819fb6ebb609cce
Patryk94 <pat_rik94@hotmail.com>
2025-06-09 17:31:52 +0200

    main.js updated and favicon copied

--- frontend/main.js
+++ frontend/main.js
@@ -2,7 +2,7 @@ windows.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
 
-const functionApi = '';
+const functionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
